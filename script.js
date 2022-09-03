7b2bee30c935c85fc3f21935df33ad4cb10d4358
djweasle <armansarg01@gmail.com>
2022-09-03 14:21:03 +0400

    changing name of repository and , anddin function behing the text

--- script.js
+++ script.js
@@ -4,22 +4,29 @@ let salary = document.getElementById('salary');
 let type = document.getElementsByName("typeName");
 let typeBox = document.getElementById("typeBox");
 let profit = document.getElementById("profit");
+
+function avance() {
+    a.id = 'avanceResult';
+    profit.removeChild(profit.lastChild);
+    profit.appendChild(a);    
+    console.log(profit); 
+}
+function salary() {
+    a.id = 'salaryResult';
+    profit.removeChild(profit.lastChild);
+    profit.appendChild(a);    
+    console.log(profit); 
+}
+
 addEventListener('click', function (e) {
     let a = this.document.createElement('div');
     if(e.target.type == 'radio'){
         switch (e.target.id) {
             case 'avance':
-                a.id = 'avanceResult';
-                profit.removeChild(profit.lastChild);
-                profit.appendChild(a);    
-                console.log(profit); 
+                avance();
                 break;
             case 'salary':
-
-                a.id = 'salaryResult';
-                profit.removeChild(profit.lastChild);
-                profit.appendChild(a);    
-                console.log(profit); 
+                salary();
                 break;
             default:
                 break;
