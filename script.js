27f844c0430767eb46d0a40a20be9eec58300806
djweasle <armansarg01@gmail.com>
2022-09-03 14:16:15 +0400

    adding styles and first functionalit

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-let a = 19;
-console.log(a);
+// let typeBtn = document.getElementById('typeButton');
+let avance = document.getElementById('avance');
+let salary = document.getElementById('salary');
+let type = document.getElementsByName("typeName");
+let typeBox = document.getElementById("typeBox");
+let profit = document.getElementById("profit");
+addEventListener('click', function (e) {
+    let a = this.document.createElement('div');
+    if(e.target.type == 'radio'){
+        switch (e.target.id) {
+            case 'avance':
+                a.id = 'avanceResult';
+                profit.removeChild(profit.lastChild);
+                profit.appendChild(a);    
+                console.log(profit); 
+                break;
+            case 'salary':
+
+                a.id = 'salaryResult';
+                profit.removeChild(profit.lastChild);
+                profit.appendChild(a);    
+                console.log(profit); 
+                break;
+            default:
+                break;
+        }
+    }
+    else return;
+})
